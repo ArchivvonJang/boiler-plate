373685ff44b430fe254db91856a75c00e952c913
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-03-09 03:20:10 +0900

    Redux

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+//import user from './user_reducer';
+
+
+const rootReducer = combineReducers({
+    //user,
+})
+
+export default rootReducer; 
+//export default 로 기본 내보내기 하고있기 때문에 다른 파일에서 가져올 때는 꼭 rootReducer 라는 이름이 아니라 다른 이름으로 바꿔서 가져올 수 있다.
+//'default' 키워드가 없이 내보냈다면 가져올 때는 import { rootReducer } from './_reducers' 처럼 중괄호로 감싸서 export 할 때의 이름을 그대로 가져와야 한다.
+//참고로 하나의 파일에서 내보내기를 할때 export default 는 한번만 쓸 수 있고, export 로 정해진 이름으로 내보낼 때는 횟수 제한 없이 내보낼 수 있다.
--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,34 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css'; 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
 
-
+// middleware를 이용해야 객체의 action만 받는 store를 promise와 function도 받게 하기 위해서 추가한다.
+// store는 객체만 받기 때문에, promise와 function도 받기 위해서 (promiseMiddleware, ReduxThunk) 추가한다.
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <Provider
+      store={createStoreWithMiddleWare(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+    >
+
+      <App />
+
+    </Provider>
+   
+
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
